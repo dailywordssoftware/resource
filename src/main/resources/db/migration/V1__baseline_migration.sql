2e8208b638381da73a3bebfbfd34996fe5dee4ae
Cagan Sit <cagan.sit@gmail.com>
2022-08-19 00:13:47 +0300

    update tags ci.yml.

--- src/main/resources/db/migration/V1__baseline_migration.sql
+++ src/main/resources/db/migration/V1__baseline_migration.sql
@@ -1,18 +1,3 @@
-CREATE TABLE flyway_schema_history
-(
-    installed_rank INTEGER       NOT NULL,
-    version        VARCHAR(50),
-    description    VARCHAR(200)  NOT NULL,
-    type           VARCHAR(20)   NOT NULL,
-    script         VARCHAR(1000) NOT NULL,
-    checksum       INTEGER,
-    installed_by   VARCHAR(100)  NOT NULL,
-    installed_on   TIMESTAMP WITHOUT TIME ZONE DEFAULT NOW() NOT NULL,
-    execution_time INTEGER       NOT NULL,
-    success        BOOLEAN       NOT NULL,
-    CONSTRAINT flyway_schema_history_pk PRIMARY KEY (installed_rank)
-);
-
 CREATE TABLE batch_job_instance
 (
     job_instance_id BIGINT       NOT NULL,
@@ -21,8 +6,6 @@ CREATE TABLE batch_job_instance
     job_key         VARCHAR(32)  NOT NULL,
     CONSTRAINT batch_job_instance_pkey PRIMARY KEY (job_instance_id)
 );
-
-CREATE INDEX flyway_schema_history_s_idx ON flyway_schema_history (success);
 
 ALTER TABLE batch_job_instance
     ADD CONSTRAINT job_inst_un UNIQUE (job_name, job_key);
