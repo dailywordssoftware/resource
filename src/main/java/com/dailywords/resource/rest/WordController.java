012db8d1b293841a47f7a1e086a29c5f0ebac041
Cagan Sit <cagan.sit@gmail.com>
2022-08-19 23:39:03 +0300

    add request mapping to word controller.

--- src/main/java/com/dailywords/resource/rest/WordController.java
+++ src/main/java/com/dailywords/resource/rest/WordController.java
@@ -7,11 +7,13 @@ import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RequestMapping("/api/v1/words")
 @AllArgsConstructor
+@RestController
 public class WordController {
     private final WordRepository wordRepository;
 
